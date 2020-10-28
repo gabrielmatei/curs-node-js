d89c2d4268a9fdfa523bfc3b7681c6f28234f5dc
Gabriel Matei <gabrielvmatei@gmail.com>
2020-10-28 18:20:00 +0200

    jwt auth

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 const express = require('express')
-const Furminator = require('./Furminator')
-const hackNSA = require('./MrRobot')
-
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
 
-app.get('/cat-fact', async (req, res) => {
-  const furminator = new Furminator()
-  const body = furminator.getFact()
-  furminator.factCount = 4
-  console.log(furminator.factCount)
-  body.then((body) => {
-    res.send(body.text)
+const config = {
+  secretKey: 'SuperSecretKey'
+}
+
+app.use(express.json())
+
+const authorizationMiddleware = (req, res, next) => {
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({
+        status: 'not ok'
+      })
+  }
+
+  const jwtToken = authorization.replace('Bearer ', '')
+
+  jwt.verify(jwtToken, config.secretKey, (err, decoded) => {
+    if (err) {
+      res
+        .status(401)
+        .send({
+          status: 'not ok'
+        })
+    } else {
+      next()
+    }
+  })
+}
+
+app.post('/graphql', authorizationMiddleware, (req, res) => {
+  res.send({
+    status: 'ok'
   })
 })
 
-app.get('/hack', async (req, res) => {
-  const furminator = new Furminator()
-
-  const { password } = await hackNSA()
-  const { text } = await furminator.getFact()
-
-  res.send({
-    password,
-    text
-  })
+app.post('/graphql/public', (req, res) => {
+  const { user, pass } = req.body
+  if (user === 'gogu' && pass === '123456') {
+    jwt.sign({}, config.secretKey, (err, token) => {
+      res.send({
+        token
+      })
+    })
+  } else {
+    res
+      .status(401)
+      .send({
+        status: 'not ok'
+      })
+  }
 })
 
 app.listen(port, _ => {
